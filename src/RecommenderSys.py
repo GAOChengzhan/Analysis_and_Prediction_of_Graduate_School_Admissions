bed90e8863b1c0012fb593f13d1a0bfe02718999
solityde826 <1812535185@qq.com>
2023-03-09 17:20:37 -0800

    update ANNmodel

--- src/RecommenderSys.py
+++ src/RecommenderSys.py
@@ -1,9 +1,10 @@
 # import modules ---------------------------------------------------------------------------------
 import pickle
+import pandas as pd
 import pathlib
 import os
 # define the recommenderSys func------------------------------------------------------------------
-def recommenderSys(loaded_model,gpa,gre_v,gre_q,gre_w,major,degree):
+def recommenderSys(uni_name,loaded_model,gpa,gre_v,gre_q,gre_w,major,degree,N=10):
     '''
     This function aims to take in the input information of the students and uses the trained model 
     to list the universities with high admission probability as recommendation universities, which
@@ -26,13 +27,29 @@ def recommenderSys(loaded_model,gpa,gre_v,gre_q,gre_w,major,degree):
     assert gre_v>=0 and gre_v<=170
     assert gre_w>=0 and gre_w<=5.0 and gre_w%0.5==0
     assert isinstance(major,str) and isinstance(degree,str)
+    cnt=0
     recommendations=[]
+
+    for insititution in uni_name:
+        if(loaded_model()):
+            recommendations.append(insititution)
+        cnt+=1
+        if(cnt>=N):
+            break
+
+    
     return recommendations
 
 
 if __name__=="__main__":
+    # load universities---------------------------------------------------------------------------
+    university_rank=pd.read_csv('./data/university_rank/university_rank.csv',names=\
+        ['rank','Uni_Name','Country','0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
+    uni_name=[]
+    for i in range(len(university_rank)):
+        uni_name.append(university_rank.iloc[i][1])
+
     # load model----------------------------------------------------------------------------------
-    
     filename=r"./model/best_model.model"
     try:
         with open(filename, 'rb') as f:
@@ -42,29 +59,30 @@ if __name__=="__main__":
     
     HEADER="According to the recommendation system, the following listed universities are for you\
         to consider:"
-    # input---------------------------------------------------------------------------------------
-    gpa = input("GPA(out of 4.0):")
-    gre_v = input("GRE verbal score:")
-    gre_q = input("GRE quantitative score:")
-    gre_w = input("GRE writing score:")
-    major = input("Major:")
-    degree = input("Degree:")
+    # # input---------------------------------------------------------------------------------------
+    # gpa = input("GPA(out of 4.0):")
+    # gre_v = input("GRE verbal score:")
+    # gre_q = input("GRE quantitative score:")
+    # gre_w = input("GRE writing score:")
+    # major = input("Major:")
+    # degree = input("Degree:")
 
-    # input validation----------------------------------------------------------------------------
-    try:
-        gpa=float(gpa)
-        gre_v=float(gre_v)
-        gre_q=float(gre_q)
-        gre_w=float(gre_w)
-    except:
-        print("Please type in valid numbers!")
-    assert gpa>=0 and gpa <=4.0
-    assert gre_q>=0 and gre_q<=170
-    assert gre_v>=0 and gre_v<=170
-    assert gre_w>=0 and gre_w<=5.0 and gre_w%0.5==0
+    # # input validation----------------------------------------------------------------------------
+    # try:
+    #     gpa=float(gpa)
+    #     gre_v=float(gre_v)
+    #     gre_q=float(gre_q)
+    #     gre_w=float(gre_w)
+    # except:
+    #     print("Please type in valid numbers!")
+    # assert gpa>=0 and gpa <=4.0
+    # assert gre_q>=0 and gre_q<=170
+    # assert gre_v>=0 and gre_v<=170
+    # assert gre_w>=0 and gre_w<=5.0 and gre_w%0.5==0
 
     # run recommendation function ---------------------------------------------------------------
-    recommendedUni=recommenderSys(loaded_model,gpa,gre_v,gre_q,gre_w,major,degree)
+    recommendedUni=recommenderSys(uni_name,loaded_model,gpa=4.0,gre_v=165,gre_q=165,gre_w=4.0,\
+                                  major="Computer Science",degree="Masters")
     print(HEADER)
 
     # list the results --------------------------------------------------------------------------
